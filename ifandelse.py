28ade3f1feb947519bda01ee37c4a92f05b5fcbe
ryansstudent <144918673+ryansstudent@users.noreply.github.com>
2023-12-08 08:55:08 -0300

    Add files via upload

---
+++ ifandelse.py
@@ -0,0 +1,13 @@
+n1 = float(input('escreva a nota: '))
+n2 = float(input('escreva a nota: '))
+n3 = float(input('escreva a nota: '))
+n4 = float(input('escreva a nota: '))
+
+media = (n1+n2+n3+n4)/4
+
+if media>= 6:
+    print('Aprovado')
+
+else:
+    print('reprovado')
+print(media)
